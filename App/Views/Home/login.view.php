457cfeb56bf22241250020f12c227d1d1ade4f40
tomin <tominotq@gmail.com>
2021-12-01 15:04:11 +0100

    opraveny register button

--- App/Views/Home/login.view.php
+++ App/Views/Home/login.view.php
@@ -4,11 +4,12 @@
 <div class="w3-container w3-light-grey" style="padding:128px 16px" id="login">
     <h2 class="centertext">LOG IN</h2>
     <div style="margin-top:48px">
+        <!-- -->
         <form method="post" action="?c=auth&a=login">
             <p><input class="w3-input w3-border" type="text" placeholder="Login" name="Login" required></p>
             <p><input class="w3-input w3-border" type="password" placeholder="Password" name="Password" required></p>
             <p>
-                <button class="w3-button w3-black" type="submit">
+                <button class="w3-button w3-black " type="submit">
                     <a class="fa fa-paper-plane"></a>
                     <a class="text">Sign in</a>
                 </button>
@@ -17,11 +18,10 @@
     </div>
 </div>
 <!-- TO DO: predizajnovat register div ? -->
-<div class="w3-container w3-light-grey regdiv" id="register">
-    <button class="w3-button w3-black right decnone bottom" type="button">
-        <a class="fa fa-paper-plane "></a>
-        <a href="?c=home&a=register" class="text">Register</a>
-    </button>
+<div  class="w3-container w3-light-grey regdiv" id="register">
+        <a href="?c=home&a=register" class="w3-button w3-black right text bottom regbut" type="button">
+            <p class="fa fa-paper-plane"></p>
+            Register
+        </a>
     <a class="right specialtext bottom">You dont have account yet?</a>
 </div>
-
